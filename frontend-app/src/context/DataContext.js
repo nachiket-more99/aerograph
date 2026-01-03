9429ed4535bff9dca63d160fab2fa7cb1691326f
Nachiket More <nachiketmore.more@gmail.com>
2026-01-03 15:00:00 +0530

    fix context error

--- frontend-app/src/context/DataContext.js
+++ frontend-app/src/context/DataContext.js
@@ -1,57 +1,33 @@
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5050';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
   const [airlinesData, setAirlinesData] = useState(null);
   const [flightsData, setFlightsData] = useState(null);
-  
   const [airportsData, setAirportsData] = useState(null);
 
   useEffect(() => {
-    // Fetch data from your API here
+    axios.get(`${BASE_URL}/airlines/all`)
+      .then((response) => setAirlinesData(response.data))
+      .catch((error) => console.error('Error fetching airlines data:', error));
 
-    const fetchAirlineData = async () => {
-      if (airlinesData) return;
-      axios.get(`${BASE_URL}/airlines/all`)
-      .then((response) => {
-        setAirlinesData(response.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching airlines data:', error);
-      });
-    };
+    axios.get(`${BASE_URL}/flights/all`)
+      .then((response) => setFlightsData(response.data))
+      .catch((error) => console.error('Error fetching flights data:', error));
 
-    const fetchFlightData = async () => {
-      if (flightsData) return;
-      axios.get(`${BASE_URL}/flights/all`)
-      .then((response) => {
-        setFlightsData(response.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching flights data:', error);
-      });
-    };
+    axios.get(`${BASE_URL}/airports/all`)
+      .then((response) => setAirportsData(response.data))
+      .catch((error) => console.error('Error fetching airports data:', error));
+  }, []);
 
-    const fetchAirportData = async () => {
-      if (airportsData) return;
-      axios.get(`${BASE_URL}/airports/all`)
-      .then((response) => {
-        setAirportsData(response.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching airports data:', error);
-      });
-    };
-
-    fetchAirlineData();
-    fetchFlightData();
-    fetchAirportData();
-  }, []); // Empty dependency array ensures the effect runs only once
-
-  return <DataContext.Provider value={{airlinesData, airportsData, flightsData}}>{children}</DataContext.Provider>;
+  return (
+    <DataContext.Provider value={{ airlinesData, airportsData, flightsData }}>
+      {children}
+    </DataContext.Provider>
+  );
 };
 
 export { DataContext, DataProvider };
